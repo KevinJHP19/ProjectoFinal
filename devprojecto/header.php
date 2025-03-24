67e355148759bc41f468a59ab807f2b1786f8372
KevinJHP19 <huamanpintokevin@fpllefia.com>
2025-03-24 17:25:32 +0000

    incluyendo el footer y header al index.php

---
+++ devprojecto/header.php
@@ -0,0 +1,24 @@
+<header>
+        <div class="container-fluid bg-dark">
+            <nav class="container navbar navbar-expand-lg navbar-dark ">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">ProjectoFinal</a>
+
+                    <div class="search-input position-relative w-50">
+                        <input type="search" class="form-control form-control-lg ps-5 bg-secondary" placeholder="Search images here">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="position-absolute top-50 translate-middle-y search-icon" width="25" height="25" viewBox="0 0 20 20" fill="currentColor">
+                           <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                        </svg>
+                     </div>
+                     <div>
+                        <a href="register.html" class="btn btn-primary">Registrate</a>
+                        <a href="login.html" class="btn btn-secondary">Iniciar Sesion</a>
+                     </div>
+                    
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav>
+        </div>
+    </header>
