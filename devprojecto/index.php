e6dc0121138f545f79840db140c066eb229ca7b9
KevinJHP19 <huamanpintokevin@fpllefia.com>
2025-05-15 14:55:54 +0000

    Paginacion hecha

--- devprojecto/index.php
+++ devprojecto/index.php
@@ -5,8 +5,24 @@
         $userQuery = $mysqli->query("SELECT * FROM USUARIOS where id = " . $_SESSION['user_id']);
         $user = $userQuery->fetch_assoc();
     }
-    $imagenes = $mysqli->query("SELECT IMAGENES.*, USUARIOS.id AS id_usuario, USUARIOS.nombre, USUARIOS.nick, USUARIOS.rol, USUARIOS.apellido, USUARIOS.avatar FROM IMAGENES LEFT JOIN USUARIOS ON IMAGENES.id_usuario = USUARIOS.id;");
+
+
+    $limit = 9; //imagenes por pagina
+    $page = isset($_GET['page']) ? $_GET['page'] : 1; //pagina actual
+    $page =  max($page,1);
+    $offset = ($page - 1) * $limit;
+
+    // Obtener el total de imágenes
+$total_result = $mysqli->query("SELECT COUNT(*) as total FROM IMAGENES");
+$total_imagenes = $total_result->fetch_assoc()['total'];
+$total_pages = ceil($total_imagenes / $limit);
+
+    $imagenes = $mysqli->query("SELECT IMAGENES.*, USUARIOS.id AS id_usuario, USUARIOS.nombre, USUARIOS.nick, USUARIOS.rol, USUARIOS.apellido, USUARIOS.avatar FROM IMAGENES LEFT JOIN USUARIOS ON IMAGENES.id_usuario = USUARIOS.id
+    ORDER BY IMAGENES.id DESC 
+    LIMIT $limit OFFSET $offset;");
     $imagenes = $imagenes->fetch_all(MYSQLI_ASSOC);
+
+
 
 ?>
 
@@ -29,12 +45,10 @@
                     <div class="col-12 text-center text-white">
                         <h1>Proyecto final</h1>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
-                        <div class="search-input position-relative">
-                                <div class="d-flex align-items-center justify-content-center w-50 mx-auto">
-                                <input type="search" class="form-control ps-2 bg-secondary mt-2 mb-2" placeholder="Buscar...">
-                                <button class="btn btn-primary p-2 ps-4 pe-4 mt-2 mb-2"><i class="fa-solid fa-magnifying-glass"></i></button>
-                            </div>
-                         </div>
+                            <div class="d-flex align-items-center justify-content-center w-50 mx-auto">
+                        <input type="search" class="form-control ps-2 bg-secondary mt-2 mb-2" placeholder="Buscar...">
+                        <button class="btn btn-primary p-2 ps-4 pe-4 mt-2 mb-2"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
                     </div>
                 </div>
             </div>
@@ -58,161 +72,65 @@
                         <div class="datos d-flex align-items-center">
                             <?php
                             //Esto es para verificar si el usuario ya le dio like a la imagen
-                            $verificarlike = $mysqli->query("SELECT * FROM LIKES WHERE imagen_id = " . $imagen['id'] . " AND usuario_id = " . $_SESSION['user_id']);
-                            $verificarlike = $verificarlike->fetch_all(MYSQLI_ASSOC);
+                            if(isset($_SESSION['user_id'])){
+                                $verificarlike = $mysqli->query("SELECT * FROM LIKES WHERE imagen_id = " . $imagen['id'] . " AND usuario_id = " . $_SESSION['user_id']);
+                                $verificarlike = $verificarlike->fetch_all(MYSQLI_ASSOC);
+                                if(!empty($verificarlike)){
+                                   echo "<a href=./funciones/disminuirlikes.php?id_imagen=". $imagen['id']."><i class='fa-solid fa-thumbs-up'></i></a>
+                                   <span>". $imagen['num_likes']." </span>";
+
+                                }else{
+                                  echo "<a href=./funciones/aumentarlikes.php?id_imagen=" .$imagen['id']."><i class='fa-regular fa-thumbs-up'></i></a>
+                                  <span>". $imagen['num_likes']." </span>";     
+                                }
+                                
+                            }else{
+                                echo "<a href=./login.php><i class='fa-solid fa-thumbs-up'></i></a>
+                                       <span>". $imagen['num_likes']." </span>";
+                            }
+                            
                             ?>
-                            <?php if(!empty($verificarlike)): ?>
-                                <a href="./funciones/disminuirlikes.php?id_imagen=<?php echo $imagen['id']?>"><i class="fa-solid fa-thumbs-up"></i></a>
-                                
-                            <?php else: ?>
-                                <a href="./funciones/aumentarlikes.php?id_imagen=<?php echo $imagen['id']?>"><i class="fa-regular fa-thumbs-up"></i></a>
-                                
-                            <?php endif; ?>
-                            <span><?php echo $imagen['num_likes'] ?></span>
                         </div>
                     </div>
                 </div>
                 <?php endforeach; ?>
 
                 
-                <div class="col-md-4 mb-4">
-                <a href="./imagen.php"><img src="./imagenes/image1.jpg" alt="Image 1" class="w-100"></a>
-                <div class="info d-flex align-items-center justify-content-between p-3" style="background-color: #141414;">
-                    <div class="izquierda d-flex align-items-center">
-                    <a href="./usuario/perfil.php"><img src="./imagenes/avatar.png" alt="" style="width: 50px; height: 50px;"></a>
-                    <h6>User Name</h6>
-                    </div>
-                    <div class="datos d-flex align-items-center">
-                    <a href="#"><i class="fa-regular fa-thumbs-up"></i></a>
-                    <span>100</span>
-                    </div>
-                </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                <img src="./imagenes/image2.jpg" alt="Image 2" class="w-100">
-                <div class="info d-flex align-items-center justify-content-between p-3" style="background-color: #141414;">
-                    <div class="izquierda d-flex align-items-center">
-                    <img src="./imagenes/avatar.png" alt="" style="width: 50px; height: 50px;">
-                    <h6>User Name</h6>
-                    </div>
-                    <div class="datos d-flex align-items-center">
-                    <i class="fa-regular fa-thumbs-up"></i>
-                    <span>100</span>
-                    </div>
-                </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                <img src="./imagenes/image3.jpg" alt="Image 3" class="w-100">
-                <div class="info d-flex align-items-center justify-content-between p-3" style="background-color: #141414;">
-                    <div class="izquierda d-flex align-items-center">
-                    <img src="./imagenes/avatar.png" alt="" style="width: 50px; height: 50px;">
-                    <h6>User Name</h6>
-                    </div>
-                    <div class="datos d-flex align-items-center">
-                    <i class="fa-regular fa-thumbs-up"></i>
-                    <span>100</span>
-                    </div>
-                </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                <img src="./imagenes/image4.jpg" alt="Image 4" class="w-100">
-                <div class="info d-flex align-items-center justify-content-between p-3" style="background-color: #141414;">
-                    <div class="izquierda d-flex align-items-center">
-                    <img src="./imagenes/avatar.png" alt="" style="width: 50px; height: 50px;">
-                    <h6>User Name</h6>
-                    </div>
-                    <div class="datos d-flex align-items-center">
-                    <i class="fa-regular fa-thumbs-up"></i>
-                    <span>100</span>
-                    </div>
-                </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                <img src="./imagenes/image5.jpg" alt="Image 5" class="w-100 ">
-                <div class="info d-flex align-items-center justify-content-between p-3" style="background-color: #141414;">
-                    <div class="izquierda d-flex align-items-center">
-                    <img src="./imagenes/avatar.png" alt="" style="width: 50px; height: 50px;">
-                    <h6>User Name</h6>
-                    </div>
-                    <div class="datos d-flex align-items-center">
-                    <i class="fa-regular fa-thumbs-up"></i>
-                    <span>100</span>
-                    </div>
-                </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                <img src="./imagenes/image6.jpg" alt="Image 6" class="w-100">
-                <div class="info d-flex align-items-center justify-content-between p-3" style="background-color: #141414;">
-                    <div class="izquierda d-flex align-items-center">
-                    <img src="./imagenes/avatar.png" alt="" style="width: 50px; height: 50px;">
-                    <h6>User Name</h6>
-                    </div>
-                    <div class="datos d-flex align-items-center">
-                    <i class="fa-regular fa-thumbs-up"></i>
-                    <span>100</span>
-                    </div>
-                </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                <img src="./imagenes/image7.jpg" alt="Image 7" class="w-100">
-                <div class="info d-flex align-items-center justify-content-between p-3" style="background-color: #141414;">
-                    <div class="izquierda d-flex align-items-center">
-                    <img src="./imagenes/avatar.png" alt="" style="width: 50px; height: 50px;">
-                    <h6>User Name</h6>
-                    </div>
-                    <div class="datos d-flex align-items-center">
-                    <i class="fa-regular fa-thumbs-up"></i>
-                    <span>100</span>
-                    </div>
-                </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                <img src="./imagenes/image8.jpg" alt="Image 8" class="w-100">
-                <div class="info d-flex align-items-center justify-content-between p-3" style="background-color: #141414;">
-                    <div class="izquierda d-flex align-items-center">
-                    <img src="./imagenes/avatar.png" alt="" style="width: 50px; height: 50px;">
-                    <h6>User Name</h6>
-                    </div>
-                    <div class="datos d-flex align-items-center">
-                    <i class="fa-regular fa-thumbs-up"></i>
-                    <span>100</span>
-                    </div>
-                </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                <img src="./imagenes/image9.jpg" alt="Image 9" class="w-100">
-                <div class="info d-flex align-items-center justify-content-between p-3" style="background-color: #141414;">
-                    <div class="izquierda d-flex align-items-center">
-                    <img src="./imagenes/avatar.png" alt="" style="width: 50px; height: 50px;">
-                    <h6>User Name</h6>
-                    </div>
-                    <div class="datos d-flex align-items-center">
-                    <i class="fa-regular fa-thumbs-up"></i>
-                    <span>100</span>
-                    </div>
-                </div>
+                
                 </div>
             </div>
             </div>
         </section>
         <section id="paginacion" class="p-3" style="background-color: rgb(33, 37, 41);">
-            <div class="text-center" >
-                <ul class="d-flex  rounded-5 " >
-                    <li class="nav-item"><a href="#">1</a></li>
-                    <li><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                    <li><a href="#">4</a></li>
-                    <li><a href="#">5</a></li>
-                    <li><a href="#">6</a></li>
-                    <li><a href="#">...</a></li>
-                    <li><a href="#">10</a></li>
-                    <li><a href="#">>></a></li>
+  <div class="text-center">
+    <ul class="pagination justify-content-center">
 
-                </ul>
-            </div>
+      <?php if ($page > 1): ?>
+        <!-- Botón para ir a la página anterior (se muestra solo si no estamos en la primera página) -->
+        <li class="page-item">
+          <a class="page-link" href="?page=<?= $page - 1 ?>">Anterior</a>
+        </li>
+      <?php endif; ?>
 
+      <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+        <!-- Botones numerados para cada página -->
+        <!-- Si la página actual coincide con $i, se marca como activa -->
+        <li class="page-item <?= ($i == $page) ? 'active' : '' ?>">
+          <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+        </li>
+      <?php endfor; ?>
 
-        </section>
+      <?php if ($page < $total_pages): ?>
+        <!-- Botón para ir a la página siguiente (se muestra solo si no estamos en la última página) -->
+        <li class="page-item">
+          <a class="page-link" href="?page=<?= $page + 1 ?>">Siguiente</a>
+        </li>
+      <?php endif; ?>
+
+    </ul>
+  </div>
+</section>
+
         
         
     </main>
